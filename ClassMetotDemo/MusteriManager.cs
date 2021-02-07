3ac8eb6026fe27b1a06d2062d31162766cd20892
Serpil AZMAN <serpil.azman12@gmail.com>
2021-02-07 20:05:53 +0300

    3.Gun Odev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+"İsimli müsteri eklendi");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" isimli kişi silindi");
+        }
+
+        public void MusteriGuncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+"Adlı kişiye ait"+musteri.Adres+" Bilgisi güncellendi");
+        }
+
+        public void MusteriListele(Musteri[] musteris)
+        {
+            foreach (var musteri in musteris)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.Adres);
+                
+
+
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,45 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri() { Id = 1, Ad = "Ali", Soyad = "Kara", Adres = "İstanbul" };
+            Musteri musteri2 = new Musteri() { Id = 2 , Ad = "Ayşe" , Soyad = "Kara" , Adres = "İzmir" };
+            Musteri musteri3 = new Musteri() { Id = 3, Ad = "Talip", Soyad = "Altın", Adres = "İzmir" };
+            Musteri musteri4 = new Musteri() { Id = 4, Ad = "Şükran", Soyad = "Ovalı", Adres = "Kastamonu" };
+            Musteri musteri5 = new Musteri() { Id = 5, Ad = "Dilara", Soyad = "Koçak", Adres = "Ankara" };
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri[] musteriler = new Musteri[] {musteri1,musteri2,musteri3,musteri4,musteri5 };
+
+            Console.WriteLine("----Musteriler Ekleniyor----");
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriEkle(musteri4);
+            musteriManager.MusteriEkle(musteri5);
+
+            Console.WriteLine();
+
+            Console.WriteLine("----Musteriler Listeleniyor----");
+
+            musteriManager.MusteriListele(musteriler);
+
+            Console.WriteLine();
+
+            Console.WriteLine(musteri1.Ad+" isimli müşteri siliniyor");
+
+            musteriManager.MusteriSil(musteri1);
+
+            Console.WriteLine();
+
+            Console.WriteLine(musteri3.Id+" Id'li "+musteri3.Ad+"isimli müşterinin adresi güncelleniyor");
+
+            musteriManager.MusteriGuncelle(musteri3);
+
+
+
+
         }
     }
 }
